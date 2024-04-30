ae56d62d1b43a21d39bd0b19d18b35619d504281
joelviljamaa <joel.viljamaa06@gmail.com>
2024-04-30 13:27:57 +0300

    jsrocket4

--- jsrocket/js/script.js
+++ jsrocket/js/script.js
@@ -1,4 +1,4 @@
-var doCoolStuff = function () {
+/*var doCoolStuff = function () {
     var currentClassName = document.getElementById('cool').className;
 
     if (currentClassName == 'cool') {
@@ -7,73 +7,40 @@ var doCoolStuff = function () {
         document.getElementById('cool').className = 'cool'
     }jkljkl
 
-    //changing class namedocument.getElementById('cool').className = 'cool red';
-}
-var sayMyName = function (name) {
-    alert('My name is: '+name);
-};
-
-var car = {
-    make: 'VW',
-    type: 'Polo',
-    color: 'blue',
-    isTurnedOn: false,
-    numberOfWheels: 4,
-    seats: [
-        'seat1',
-        'seat2',
-        'seat3',
-        'seat4',
-],
-turnOn: function () {
-    this.isTurnedOn = true;
-},
-fly: function () {
-    alert('fly');
-},
-switchCar: function (isOn) {
-    console.log('turn car'+isOn)
-        ;
-    if (isOn = true) {
-        this.isTurnedOn = true;
-    } else {
-        thsi.isTurnedOn = false;
-    }
-}
-};
-
+    changing class namedocument.getElementById('cool').className = 'cool red';
+*/
 var timer = null;
 var countdownNumber = 10;
 
-var changeState = function (state) {
+var changeState= function(state){
     document.body.className = 'body-state'+ state;
-
-    if (state == 2) {
-        timer = setInterval(function () {
-            document.getElementById('countdown').innerHTML = countdownNumber;
+    clearInterval(timer);
+    if(state == 2) {
+        timer = setInterval(function() {
             countdownNumber = countdownNumber -1;
-            if (countdownNumber <= 0) {
+            document.getElementById('countdown').innerHTML= countdownNumber;
+            
+            if (countdownNumber <= 0){
                 changeState(3);
-            };
-        },1000);
-        else if (state == 3); {
-            var success = setTimeout(function ())
-            {
-                var randomNumber = math.round
-                (Math.random()*10)
+            }
+        }, 200);
 
+    }      
+    else if (state == 3) {
+            var success = setTimeout (function(){
+
+                var randomNumber = Math.round(Math.random()*10);
+
+                console.log('randomNumber:',randomNumber)
+                // success
                 if (randomNumber > 5) {
-                    //do smthn
+                    changeState(4);
+
+                } else {
+                    changeState(5); //oh no!
                 }
-                else {
-                    //do smthn else
-                }
-            } 
-        }
-    };
-}
+            }, 2000);
 
 
-
-
-console.log('hello there friends!');
+        };
+    }
